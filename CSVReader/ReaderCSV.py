cda23eeceea8c8c7ba43d3235d727011002332a7
RosaMoss <85589945+RosaMoss@users.noreply.github.com>
2021-07-15 09:05:28 -0400

    Modified functions and tests to allow for testing

--- CSVReader/ReaderCSV.py
+++ CSVReader/ReaderCSV.py
@@ -1,17 +1,14 @@
 import csv
 from FileUtilities import absolutepath
-'''
-constructs a class at runtime like (class_name, (objects,), dictionary) 
-'''
-def classfactory(class_name, dictionary):
-    return type(class_name, (object,), dictionary)
+import csv
+from Fileutilities.absolutepath import absolute_path
 
+
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
 '''
 helper method return_data_as_objs
-@:param class name
-@:return an object list ie for addition: type('addition',()  )
 '''
-
 def return_data_as_objs(classname, dictData):
     listofobjs = []
     if len(dictData) == 0:
@@ -21,21 +18,9 @@ def return_data_as_objs(classname, dictData):
             listofobjs.append(classfactory(classname, row))
     return listofobjs
 
-'''
-This class only reads a file in CSV
-should technically read A file regardless of filetype but mehh
-'''
-class ReaderOfCSVs:
-    data = [] #this var ruined it all for me
-    '''
-    Constructor
-    @:param filepath
-    open a file path specified for reading a csv
-    DictReader from csv reads and reads it as a dictionary per line
-    for example: {Value1: 580, Value2: 459, Result: 1039}
-    until EOF
-    Each row in the csvRow is then added to a list
-    '''
+class CsvReader:
+    data = []
+
     def __init__(self, filepapa):
         self._newData = []
         self._filepapa = filepapa
